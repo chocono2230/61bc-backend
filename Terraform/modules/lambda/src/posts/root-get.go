f45fde64906dbf435155ed2b2fdb0d5815ef8842
chocono2230 <61158776+chocono2230@users.noreply.github.com>
2023-03-11 15:13:00 +0900

    特定ユーザのPost取得APIの作成 #17 (#37)
    * userIdに基づいたPost取得APIの実装
    
    * ポリシー設定にリソース追加

--- Terraform/modules/lambda/src/posts/root-get.go
+++ Terraform/modules/lambda/src/posts/root-get.go
@@ -1,7 +1,6 @@
 package posts
 
 import (
-	"fmt"
 	"os"
 
 	"github.com/aws/aws-lambda-go/events"
@@ -16,10 +15,51 @@ func get(request events.APIGatewayProxyRequest) (any, int, error) {
 
 	switch {
 	case uid != "":
-		return nil, 400, fmt.Errorf("that feature has not been implemented")
+		return getPostFromUid(uid)
 	default:
 		return getAllPost()
 	}
+}
+
+func getPostFromUid(uid string) (any, int, error) {
+	sess, err := session.NewSession()
+	if err != nil {
+		return nil, 500, err
+	}
+	db := dynamodb.New(sess)
+
+	tn := os.Getenv("POSTS_TABLE_NAME")
+	in := os.Getenv("POSTS_TABLE_GSI_NAME_USR")
+	input := &dynamodb.QueryInput{
+		IndexName: aws.String(in),
+		TableName: aws.String(tn),
+		ExpressionAttributeNames: map[string]*string{
+			"#userId": aws.String("userId"),
+		},
+		ExpressionAttributeValues: map[string]*dynamodb.AttributeValue{
+			":userId": {
+				S: aws.String(uid),
+			},
+		},
+		KeyConditionExpression: aws.String("#userId = :userId"),
+		ScanIndexForward:       aws.Bool(false),
+	}
+	result, err := db.Query(input)
+	if err != nil {
+		return nil, 500, err
+	}
+
+	posts := []Post{}
+	err = dynamodbattribute.UnmarshalListOfMaps(result.Items, &posts)
+	if err != nil {
+		return nil, 500, err
+	}
+	response := struct {
+		Posts []Post `json:"posts"`
+	}{
+		Posts: posts,
+	}
+	return response, 200, nil
 }
 
 func getAllPost() (any, int, error) {
