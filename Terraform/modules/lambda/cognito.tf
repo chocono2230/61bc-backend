1d776293738ea454d4ed664762020595fd1d8d83
chocono2230 <61158776+chocono2230@users.noreply.github.com>
2023-03-12 16:45:05 +0900

    パスワードポリシーの変更

--- Terraform/modules/lambda/cognito.tf
+++ Terraform/modules/lambda/cognito.tf
@@ -28,8 +28,6 @@ resource "aws_cognito_user_pool" "this" {
     minimum_length                   = 6
     require_lowercase                = true
     require_numbers                  = true
-    require_symbols                  = true
-    require_uppercase                = true
     temporary_password_validity_days = 7
   }
 
